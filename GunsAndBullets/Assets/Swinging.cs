2731c54f3d66104c1c266915965a39d046573195
PedroNeves <133124266+PedroNevesDev@users.noreply.github.com>
2024-12-18 22:59:44 +0000

    More Unfinished Swinging

--- GunsAndBullets/Assets/Swinging.cs
+++ GunsAndBullets/Assets/Swinging.cs
@@ -6,6 +6,15 @@ public class Swinging : MonoBehaviour
 {
     [Header("Input")]
     public KeyCode swingKey = KeyCode.Mouse0;
+    [Header("References")]
+    public LineRenderer lr;
+    public Transform lineOringin, cam, player;
+    public LayerMask whatIsGrappleable;
+
+    [Header("Swinging")]
+    private float maxSwingDistance = 25f;
+    private Vector3 swingPoint;
+    private SpringJoint joint;
 
     void Update()
     {
@@ -14,11 +23,28 @@ public class Swinging : MonoBehaviour
     }
     private void StartSwing()
     {
+        RaycastHit hit;
+        if(Physics.Raycast(cam.position, cam.forward, out hit, maxSwingDistance, whatIsGrappleable))
+        {
+            swingPoint = hit.point;
+            joint = player.gameObject.AddComponent<SpringJoint>();
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedAnchor = swingPoint;
 
+            float distanceFromPoint = Vector3.Distance(player.position, swingPoint);
+
+            joint.maxDistance = distanceFromPoint * 0.8f;
+            joint.minDistance = distanceFromPoint * 0.25f;
+
+            joint.spring = 4.5f;
+            joint.damper = 7f;
+            joint.massScale = 4.5f;
+        }
     }
 
     private void StopSwing()
     {
-
+        lr.positionCount = 0;
+        Destroy(joint);
     }
 }
